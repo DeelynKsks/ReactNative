50eccb2827546d47d170c085f5f1d9528774f38c
DeelynKsks <zaikeny77@gmail.com>
2023-05-29 04:34:37 -0300

    Trabajando el proyecto con la API - Estancado en imágenes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { TouchableOpacity, Text, Image, View, Dimensions } from 'react-native';
 import { cardStyles } from '../Styles/CardStyles';
-<<<<<<< HEAD
 import CoolButton from './CoolButton';
 import { useNavigation } from '@react-navigation/native';
-
-=======
-import { library } from '../Library/library';
->>>>>>> f97831842c3ae0bd0d1abae1dfee09d3af905b13
 
 function Card(props) {
     const { data, onDelete, onPress } = props
@@ -26,7 +21,6 @@ function Card(props) {
     };
 
   return (
-<<<<<<< HEAD
     <TouchableOpacity
       style={[cardStyles.cardContainer, { width: windowWidth / 2 }]}
       onPress={handlePress}
@@ -34,11 +28,6 @@ function Card(props) {
         <Image style={cardStyles.cardImage} source={{ uri: data.ProfileImage}} />
         <Text style={cardStyles.name}>{data.Name}</Text>
         <CoolButton title={'Eliminar'} action={handleDelete}/>
-=======
-    <TouchableOpacity style={cardStyles.card}>
-        <Image style={cardStyles.image} source={{uri: library.extractImage(data.image)}}/>
-        <Text style={cardStyles.name}>{data.name}</Text>
->>>>>>> f97831842c3ae0bd0d1abae1dfee09d3af905b13
     </TouchableOpacity>
   )
 }
--- src/Library/library.js
+++ src/Library/library.js
@@ -7,7 +7,6 @@ export const library = {
             
         return consulta
     },
-<<<<<<< HEAD
     getCharacter: async (url, character) => {
         const response = await fetch(url, {
           method: 'POST',
@@ -18,12 +17,7 @@ export const library = {
         })
         .then(a => a.json())
         .catch(err => console.log(err))
-
+        
         return response
-=======
-    extractImage: (urlImage) => {
-        const regex = /(.png\/).*/
-        return urlImage.replace(regex, '$1')
->>>>>>> f97831842c3ae0bd0d1abae1dfee09d3af905b13
     }
 }
